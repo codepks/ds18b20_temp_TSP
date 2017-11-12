8e63c6b17ac79e043faefc90e63131fffe48050b
pkspats <faith.prashant@gmail.com>
2017-11-12 08:54:32 +0530

    First working ds18b20 code for Thingspeak

---
+++ temp.py
@@ -0,0 +1,48 @@
+import os                                                  # import os module
+import glob                                                # import glob module
+import time                                                # import time module
+import httplib
+import urllib
+import datetime
+import smbus
+from ctypes import c_short
+
+os.system('modprobe w1-gpio')                              # load one wire communication device kernel modules
+os.system('modprobe w1-therm')                                                 
+base_dir = '/sys/bus/w1/devices/'                          # point to the address
+device_folder = glob.glob(base_dir + '28*')[0]             # find device with address starting from 28*
+device_file = device_folder + '/w1_slave'                  # store the details
+def read_temp_raw():
+   f = open(device_file, 'r')
+   lines = f.readlines()                                   # read the device details
+   f.close()
+   return lines
+
+def read_temp():
+   lines = read_temp_raw()
+   while lines[0].strip()[-3:] != 'YES':                   # ignore first line
+      time.sleep(0.2)
+      lines = read_temp_raw()
+   equals_pos = lines[1].find('t=')                        # find temperature in the details
+   if equals_pos != -1:
+      temp_string = lines[1][equals_pos+2:]
+      temp_c = float(temp_string) / 1000.0                 # convert to Celsius
+      temp_f = temp_c * 9.0 / 5.0 + 32.0                   # convert to Fahrenheit 
+      return temp_c, temp_f
+
+while True:
+   temp_dig=[]
+   temp_dig=read_temp()                                      # Print temperature    
+   a = temp_dig[0]
+   b = temp_dig[1]
+   print a , b
+   params = urllib.urlencode({'field1': a,'field2': b, 'key':'UP08V8R8ELDLN9MB' }) 
+   headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+   conn = httplib.HTTPConnection("api.thingspeak.com:80")
+   conn.request("POST", "/update", params, headers)
+   response = conn.getresponse()
+   print response.status, response.reason
+   data = response.read()
+   conn.close()
+   
+   time.sleep(1)
